49cc690207cee77dcd8e38ae8df575fd0f967cae
Jonathan Ho <hojonathanho@gmail.com>
2015-07-25 18:25:30 -0700

    move to unittest for tests

--- test/test_devices.py
+++ test/test_devices.py
@@ -1,42 +1,50 @@
 import numpy as np
 import cgt
-
-cgt.set_precision('single')
-N = 10
-K = 3
-
-Xval = np.random.randn(N,K)
-wval = np.random.randn(K)
-bval = np.random.randn()
-yval = np.random.randn(N)
-
-X_nk = cgt.shared(Xval, "X", device=cgt.Device(devtype='gpu'))
-y_n = cgt.shared(yval, "y")
-w_k = cgt.shared(wval, "w")
-b = cgt.shared(bval, name="b")
-
-print "bval",bval
-
-ypred = cgt.dot(cgt.square(X_nk), w_k) + b
-
-err = cgt.sum(cgt.square(ypred - y_n))
-g = cgt.grad(err, [w_k, b])
-outputs = [err]
-def devfn(node):
-    if isinstance(node, cgt.Result) and node.op == err.op: # XXX add fn for this
-        return cgt.Device(devtype="cpu")
-
-func=cgt.VarSizeFunc([], outputs, devfn = devfn)
-
-def writedev(node,o):
-    o.write(" | device: %s"%func.node2device[node])
-
-cgt.print_tree(func.outputs, nodefn=writedev)
+import unittest
 
 
+class DevicesTestCase(unittest.TestCase):
+    def test_devices(self):
+        cgt.set_precision('single')
+        N = 10
+        K = 3
 
-print "ready..."
-numerr = func()
-print "done"
+        Xval = np.random.randn(N,K)
+        wval = np.random.randn(K)
+        bval = np.random.randn()
+        yval = np.random.randn(N)
 
-assert np.allclose(numerr,cgt.numeric_eval([err],{}))
+        X_nk = cgt.shared(Xval, "X", device=cgt.Device(devtype='gpu'))
+        y_n = cgt.shared(yval, "y")
+        w_k = cgt.shared(wval, "w")
+        b = cgt.shared(bval, name="b")
+
+        print "bval",bval
+
+        ypred = cgt.dot(cgt.square(X_nk), w_k) + b
+
+        err = cgt.sum(cgt.square(ypred - y_n))
+        g = cgt.grad(err, [w_k, b])
+        outputs = [err]
+        def devfn(node):
+            if isinstance(node, cgt.Result) and node.op == err.op: # XXX add fn for this
+                return cgt.Device(devtype="cpu")
+
+        func=cgt.VarSizeFunc([], outputs, devfn = devfn)
+
+        def writedev(node,o):
+            o.write(" | device: %s"%func.node2device[node])
+
+        cgt.print_tree(func.outputs, nodefn=writedev)
+
+
+
+        print "ready..."
+        numerr = func()
+        print "done"
+
+        np.testing.assert_allclose(numerr,cgt.numeric_eval([err],{}))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- test/test_einsum.py
+++ test/test_einsum.py
@@ -1,18 +1,27 @@
 import cgt, numpy as np,numpy.random as nr
-x = cgt.tensor3()
-y = cgt.tensor3()
+import unittest
 
-sizes = {'i':2,'j':3,'k':5,'l':7}
-xaxes = 'ijk'
-yaxes = 'ikl'
-zaxes = 'ijl'
-for i in xrange(10):
-    xperm = xaxes
-    (yperm,zperm) = permaxes = [[chars[i] for i in np.random.permutation(3)] for chars in [yaxes,zaxes]]
-    desc = "%s,%s->%s"%tuple("".join(chars) for chars in [xperm] + permaxes)
-    z = cgt.einsum(desc, x, y)
-    xval = nr.randn(*(sizes[c] for c in xperm))
-    yval = nr.randn(*(sizes[c] for c in yperm))
-    assert np.allclose(
-        cgt.numeric_eval(z, {x : xval, y : yval}) ,
-        np.einsum(desc, xval, yval))
+
+class EinsumTestCase(unittest.TestCase):
+    def test_einsum(self):
+        x = cgt.tensor3()
+        y = cgt.tensor3()
+
+        sizes = {'i':2,'j':3,'k':5,'l':7}
+        xaxes = 'ijk'
+        yaxes = 'ikl'
+        zaxes = 'ijl'
+        for i in xrange(10):
+            xperm = xaxes
+            (yperm,zperm) = permaxes = [[chars[i] for i in np.random.permutation(3)] for chars in [yaxes,zaxes]]
+            desc = "%s,%s->%s"%tuple("".join(chars) for chars in [xperm] + permaxes)
+            z = cgt.einsum(desc, x, y)
+            xval = nr.randn(*(sizes[c] for c in xperm))
+            yval = nr.randn(*(sizes[c] for c in yperm))
+            np.testing.assert_allclose(
+                cgt.numeric_eval(z, {x : xval, y : yval}),
+                np.einsum(desc, xval, yval))
+
+
+if __name__ == "__main__":
+    unittest.main()
