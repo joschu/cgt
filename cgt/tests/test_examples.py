8df4fdddd57366958003506739665aba0e2648ee
John Schulman <john.d.schulman@gmail.com>
2015-08-25 13:38:09 -0700

    don't unittest examples that require a large file to be downloaded

--- cgt/tests/test_examples.py
+++ cgt/tests/test_examples.py
@@ -9,12 +9,13 @@ def skip_example(cmd):
     raise SkipTest(cmd)
 
 def test_examples():
-    yield run_example, "CGT_FLAGS=backend=python python %s/../../examples/demo_mnist.py --unittest"%thisdir
-    yield run_example, "CGT_FLAGS=backend=native python %s/../../examples/demo_mnist.py --unittest"%thisdir
-    yield run_example, "CGT_FLAGS=backend=native python %s/../../examples/demo_cifar.py --unittest"%thisdir
+    if osp.exists("%s/../../downloads/mnist.npz"%thisdir):
+        yield run_example, "CGT_FLAGS=backend=python python %s/../../examples/demo_mnist.py --unittest"%thisdir
+        yield run_example, "CGT_FLAGS=backend=native python %s/../../examples/demo_mnist.py --unittest"%thisdir
+        yield run_example, "CGT_FLAGS=backend=native python %s/../../examples/demo_cifar.py --unittest"%thisdir
+        yield run_example, "python %s/../../examples/cgt_theano_feedforward_comparison.py --unittest"%thisdir
     yield run_example, "cd %s/../../examples/ && CGT_FLAGS=backend=native python demo_char_rnn.py --unittest"%thisdir
     yield run_example, "CGT_FLAGS=backend=native python %s/../../examples/demo_neural_turing_machine.py --unittest"%thisdir
-    yield run_example, "python %s/../../examples/cgt_theano_feedforward_comparison.py --unittest"%thisdir
     runipycmd = "runipy %s/../../examples/tutorial.ipynb"%thisdir
     try:
         import graphviz
